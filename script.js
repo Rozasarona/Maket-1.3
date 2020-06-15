3b0de2742cbcd9471a7df06e01fd6cba14a943e4
Svetlana Mamadjanova <svetlanam300@gmail.com>
2020-06-16 00:24:22 +0300

    gallery html finished

--- script.js
+++ script.js
@@ -10,18 +10,15 @@
 
     let expandButton = document.querySelector('#expand');
     let containerElement = document.querySelector('.swiper-container');
-    let myLink = document.querySelector('.additional__link .link');
-    let myImg = expandButton.querySelector('img');
 
     expandButton.addEventListener('click', function() {
-      myImg.classList.add('additional__icon');
-      if (myLink.innerHTML === "Показать все") {
-          myLink.innerHTML = "Скрыть";
-          myImg.classList.add('additional__icon');
-          containerElement.style.height = "unset";
+      if (expandButton.innerHTML === "Показать все") {
+        expandButton.innerHTML = "Скрыть";
+        expandButton.classList.add('expandlink_upside-down');
+        containerElement.style.height = "unset";
       } else {
-        myLink.innerHTML = "Показать все";
-        myImg.classList.remove('additional__icon');
+        expandButton.innerHTML = "Показать все";
+        expandButton.classList.remove('expandlink_upside-down');
         containerElement.style.height = "";
       }
     });
