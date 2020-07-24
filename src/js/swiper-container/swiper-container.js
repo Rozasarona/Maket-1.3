df895902cad54957f76af0f2f7ac4d8a0ea72d3a
Svetlana Mamadjanova <svetlanam300@gmail.com>
2020-07-24 19:23:48 +0300

    margins,sidebar,npm

--- src/js/swiper-container/swiper-container.js
+++ src/js/swiper-container/swiper-container.js
@@ -1,13 +1,13 @@
-import Swiper from 'swiper';
+import Swiper, {Pagination} from 'swiper';
+Swiper.use([Pagination]);
 
-if (window.innerWidth <= 768) {
+if (window.innerWidth < 1120) {
     let mySwiper = new Swiper ('.swiper-container', {
         slidesPerView: 'auto',
         centeredSlides: true,
         loop: true,
         pagination: {
         el: '.swiper-pagination',
-        clickable: true,
         }
     });
 }
